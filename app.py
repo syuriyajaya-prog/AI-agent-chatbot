7275bdfc2a2bcc6a04723dbddf19f4a2a3448efb
syuriyajaya-prog <syuriyajaya@gmail.com>
2026-04-06 14:24:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ Knowledge base:
 # Configure Gemini
 genai.configure(api_key="AIzaSyA_O1AF8XVd1DcFG6hh53R8euFD63WwMI4")
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash-lite",
+    model_name="gemini-2.5-flash",
     system_instruction=prompt
 )
 
